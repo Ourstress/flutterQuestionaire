b4d57e298edca8ed7bb26dd5c600a388a984ce14
SG <sgng1@e.ntu.edu.sg>
2020-01-08 12:24:39 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# IS3103 App
+
+A Flutter project for IS3103.
+
+## Resources for flutter
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IS3013 app',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('IS3103 App'),
+      ),
+      body: Center(child: Text('Welcome to the IS3013 app')),
+    );
+  }
+}
